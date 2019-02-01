c2ad449049c3505171600ef39fba4ae43807bd63
Lakshman Babu <rlakshmanbabu@deloitte.com>
2019-02-01 23:08:58 +0530

    ok check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,9 +26,5 @@ export class AppComponent implements OnInit {
     let routeString = this.route;
     this.showHF = true;
     //comments - 1
-    //comments - 1
-    //comments
-    //some code added as part of branch-1
-    //Some code added as part of branch-2
   }
 }
